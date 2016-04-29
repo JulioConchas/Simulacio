fe5f87b45a6895b650fc0d12e8bccf7293ed5522
Conchas Jimenez <julio.cesar.conchas.jimenez@intel.com>
2016-04-29 09:51:44 -0500

    addes Makefile, and menu code in main.cpp

---
+++ sistema_montecarlo/main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using namespace std;
+ 
+
+int main(){
+	
+	int option;
+
+	cout << " **** Montecarlo system **** \n";
+	cout <<"Newton raphson...............(1)\n";
+	cout <<"Simpson......................(2)\n";
+	cout <<"Secante......................(3)\n";
+	cout <<"Regula Falsi.................(4)\n";
+	cout <<"Muller.......................(5)\n";
+	cout <<"Relajacion...................(6)\n";
+	cout <<"Biseccion....................(7)\n";
+	cin >>option;
+
+		switch(option){
+		
+			case 1:
+			break;
+			case 2:
+			break;
+			case 3:
+			break;
+			case 4:
+			break;
+			case 5:
+			break;
+			case 6:
+			break;
+			case 7:
+			break;
+		}
+
+return 0;
+}
+
